48892eefc5d6ccfa1f8067bb600de7a2b31d35b6
SanW20 <sanweerasinghe241@gmail.com>
2026-04-28 15:03:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -313,4 +313,4 @@ def days_since_filter(last_watered_str: str | None) -> str:
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000, use_reloader=False)
+    app.run(debug=True, host="0.0.0.0", port=5000, use_reloader=False) 
